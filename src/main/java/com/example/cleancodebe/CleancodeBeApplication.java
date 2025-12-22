0b3b929d9ab837ff3ee609b0f0605819ce9ac7e0
Jiwoo <ziuoo0424@gmail.com>
2025-12-23 00:12:16 +0900

    init

---
+++ src/main/java/com/example/cleancodebe/CleancodeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleancodebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleancodeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleancodeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cleancodebe/CleancodeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleancodebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancodeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
