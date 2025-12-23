985a52748c2a6dfacf54bb6c7d0e1cb4546d1123
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-12-23 13:36:21 +0900

    feat : user -> student

--- src/main/java/com/example/cleancodebe/entity/Student.java
+++ src/main/java/com/example/cleancodebe/entity/Student.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class Student {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
